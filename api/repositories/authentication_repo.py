0a5b78d6f5c69b92c3ca465334d6fc3ac1e79aef
entrecanales <eccuenca2@gmail.com>
2025-05-11 11:43:42 +0200

    Password encryption with Argon2

--- api/repositories/authentication_repo.py
+++ api/repositories/authentication_repo.py
@@ -3,18 +3,22 @@ from sqlalchemy.orm import Session
 from api.model.entities.generated_models import Users as User
 from api.helpers import model_helper as ModelHelper
 from datetime import datetime, timezone
+from argon2 import PasswordHasher
 
 
 class AuthenticationRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def new_user(self, user: User) -> User | None:
+    def new_user(self, user: User):
         # initialize the created_at att to now, the time and date of the operation
         user.created_at = datetime.now(tz=timezone.utc)
         user.updated_at = datetime.now(tz=timezone.utc)
+        # encryption of the password before inserting into the db
+        ph = PasswordHasher()
+        user.password = ph.hash(user.password)
         user_dict = ModelHelper.model_to_dict(user)
-        print(user_dict)
+        # print(user_dict)
         sql = text("""
                 INSERT INTO users (
                     username, password, email, display_name, birth_date,
--- api/routes/authentication.py
+++ api/routes/authentication.py
@@ -6,12 +6,13 @@ from api.model.requests.authentication_requests import RegisterRequest
 router = APIRouter()
 
 
-@router.post("/register")
+# TODO: This send an email to the address to confim and needs to store the password ENCRYPTED
+@router.post("/register", status_code=201)
 def register(request: RegisterRequest, controller: AuthenticationController = Depends()):
     return controller.register(request)
 
 
-@router.post("/login")
+@router.post("/login")  # TODO: This needs to return a JWT token
 def login(controller: AuthenticationController = Depends()):
     return controller.login()
 
